107ef7e705ed7208e479ff018b794c3388400d60
faun3 <antonioculcea@gmail.com>
2024-01-21 23:26:06 +0200

    no idea what I did but it works

--- components/AerialHeader/AerialHeader.tsx
+++ components/AerialHeader/AerialHeader.tsx
@@ -1,26 +1,34 @@
 "use client";
-import Image from "next/image";
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
 import { useGSAP } from "@gsap/react";
 import styles from "./AerialHeader.module.scss";
 import aerial from "../../images/ring.jpg";
 import { useRef } from "react";
+gsap.registerPlugin(ScrollTrigger);
 
 export default function AerialHeader() {
   const donutRef = useRef(null);
+  const container = useRef(null);
   useGSAP(() => {
-    const tl = gsap.timeline();
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        scrub: 1,
+        pin: donutRef.current,
+        start: "top top",
+      },
+    });
     tl.to(donutRef.current, {
-      clipPath: "circle(75% at 50% 50%)",
-      duration: 1.5,
+      clipPath: "circle(70% at 50% 50%)",
       mask: "radial-gradient(circle at 50% 50%, transparent 80%, black 0%)",
-    }).to(donutRef.current, {
+    });
+    tl.to(donutRef.current, {
+      clipPath: "circle(70% at 50% 50%)",
       mask: "radial-gradient(circle at 50% 50%, transparent 100%, black 0%)",
-      duration: 1,
     });
   });
   return (
-    <div>
+    <div ref={container}>
       <div
         className={styles.imageWrapper}
         style={{
